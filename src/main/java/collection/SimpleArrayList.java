5950ed3d48d54010d913c8ded768c9f1186dc87a
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-08-08 03:06:42 +0200

    1. Динамический список на массиве. [#158]. Some changes in Simple ArrayList class methods.

--- src/main/java/collection/SimpleArrayList.java
+++ src/main/java/collection/SimpleArrayList.java
@@ -26,16 +26,14 @@ public class SimpleArrayList<T> implements SimpleList<T> {
 
     @Override
     public T set(int index, T newValue) {
-        Objects.checkIndex(index, size);
-        T result = container[index];
+        T result = get(index);
         container[index] = newValue;
         return result;
     }
 
     @Override
     public T remove(int index) {
-        Objects.checkIndex(index, size);
-        T result = container[index];
+        T result = get(index);
         System.arraycopy(container, index + 1, container, index, container.length - index - 1);
         container[container.length - 1] = null;
         size--;
@@ -84,7 +82,7 @@ public class SimpleArrayList<T> implements SimpleList<T> {
         };
     }
 
-    public void expandArray() {
-        container = Arrays.copyOf(container, container.length * 2);
+    private void expandArray() {
+        container = container.length != 0 ? Arrays.copyOf(container, container.length * 2) : (T[]) new Object[1];
     }
 }
