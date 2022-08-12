de0f1116d00988fc71cbf7a7921f38fa6c7f8574
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-08-12 21:28:59 +0200

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#71474].

--- src/main/java/collection/ForwardLinked.java
+++ src/main/java/collection/ForwardLinked.java
@@ -19,6 +19,11 @@ public class ForwardLinked<T> implements Iterable<T> {
         tail.next = node;
     }
 
+    public void addFirst(T value) {
+        ForwardLinked.Node<T> node = new ForwardLinked.Node<T>(value, head);
+        head = node;
+    }
+
     public T deleteFirst() {
         if (head == null) {
             throw new NoSuchElementException();
