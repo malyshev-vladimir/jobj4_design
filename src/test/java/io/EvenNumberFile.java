a454a949b0e68776d613195953b6009e61c7ef52
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-09-11 02:47:33 +0200

    0.2. FileInputStream [#4898]

---
+++ src/test/java/io/EvenNumberFile.java
@@ -0,0 +1,27 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                int num = Integer.parseInt(line);
+                if (num % 2 == 0) {
+                    System.out.println(line + " - even");
+                } else {
+                    System.out.println(line + " - not even");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
