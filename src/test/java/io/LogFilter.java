308373e2aaeda74011dc0c9d8de0293f3ce04965
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-09-12 01:34:09 +0200

    0.3. BufferedReader. [#252489]

---
+++ src/test/java/io/LogFilter.java
@@ -0,0 +1,29 @@
+package io;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.System.out;
+
+public class LogFilter {
+    public List<String> filter(String file) {
+        List<String> result = null;
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            result = in.lines()
+                    .map(s -> s.split(" "))
+                    .filter(s -> s[s.length - 2].equals("404"))
+                    .map(s -> String.join(" ", s))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter();
+        List<String> log = logFilter.filter("log.txt");
+        out.println(log);
+    }
+}
