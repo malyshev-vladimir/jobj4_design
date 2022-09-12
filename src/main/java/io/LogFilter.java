bd0c3fdcecdf7ce9948778f44e0e41c046bd1a61
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-09-12 23:54:34 +0200

    0.4. BufferedOutputStream [#252490].

--- src/main/java/io/LogFilter.java
+++ src/main/java/io/LogFilter.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 import static java.lang.System.out;
 
 public class LogFilter {
-    public List<String> filter(String file) {
+    public static List<String> filter(String file) {
         List<String> result = null;
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             result = in.lines()
@@ -21,9 +21,21 @@ public class LogFilter {
         return result;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String string: log) {
+                out.println(string);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        LogFilter logFilter = new LogFilter();
-        List<String> log = logFilter.filter("log.txt");
-        log.forEach(out::println);
+        List<String> log = filter("src/main/java/io/log.txt");
+        save(log, "src/main/java/io/resultOfLogFilterFromLog.txt");
     }
 }
