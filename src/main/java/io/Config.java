e664de2c60434abdf2f3d5751f25821076d16fe1
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-09-18 18:57:03 +0200

    1. Читаем файл конфигурации [#858 #343191]. Code style fix.

--- src/main/java/io/Config.java
+++ src/main/java/io/Config.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
 
 public class Config {
 
@@ -21,23 +19,25 @@ public class Config {
     public void load() {
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
             read.lines()
-                    .filter(s -> s.length() != 0 && !s.startsWith("#"))
+                    .filter(s -> !s.isBlank() && !s.startsWith("#"))
                     .map(s -> s.split("=", 2))
                     .map(line -> {
                         if (line.length != 2) {
                             throw new IllegalArgumentException();
-                        } else if (line[0].trim().length() == 0) {
-                            throw new IllegalArgumentException();
-                        }  else if (line[1].trim().length() == 0) {
-                            throw new IllegalArgumentException();
-                        } else {
-                            return line;
                         }
+                        if (line[0].isBlank()) {
+                            throw new IllegalArgumentException();
+                        }
+                        if (line[1].isBlank()) {
+                            throw new IllegalArgumentException();
+                        }
+                        return line;
                     })
                     .forEach(l -> values.put(l[0].trim(), l[1].trim()));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     public String value(String key) {
