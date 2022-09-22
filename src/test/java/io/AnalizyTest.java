11e5afb52d810f03e8410d0d576ea74588feb884
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-09-22 23:02:35 +0200

    3.0. Тестирование IO [#173905].

---
+++ src/test/java/io/AnalizyTest.java
@@ -0,0 +1,31 @@
+package io;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import java.io.*;
+import java.nio.file.Path;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class AnalizyTest {
+
+    @Test
+    void unavailable(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("500 10:59:01");
+            out.println("400 11:01:02");
+            out.println("200 11:02:02");
+        }
+        File target = tempDir.resolve("target.csv").toFile();
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(result::append);
+        }
+        assertThat("10:57:01;11:02:02;").isEqualTo(result.toString());
+    }
+}
