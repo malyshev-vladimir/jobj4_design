a47dbd5c3afaf81c9429fdcca3cc4fc7f13042ad
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-09-25 23:48:51 +0200

    4.1. Сканирование файловой системы. [#106929].

---
+++ src/main/java/io/Search.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Predicate;
+
+public class Search {
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start, p -> p.toFile().getName().endsWith(".java")).forEach(System.out::println);
+    }
+}
